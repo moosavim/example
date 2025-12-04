d75546073a6c74f4f9f5ed68b56eef76cced494f
krshme <mali.moosavi8@gmail.com>
2025-12-04 08:03:33 +0330

    add 1_gpt.py

---
+++ 1_gpt.py
@@ -0,0 +1,13 @@
+num = [2 , -3 , 0 , 4 , -5 , 1, -1 , 6]
+num.sort()
+print(num)
+n_num_pos = []
+n_num_neg = []
+for i in num:
+    if i > 0 :
+        n_num_pos.append(i)
+    if i < 0 :
+        n_num_neg.append(i)
+n_num_neg.reverse()
+n_num_neg.extend(n_num_pos)
+print(n_num_neg)
